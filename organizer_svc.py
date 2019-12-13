e907ef001f94e7906c8b514305d42d0f355878fd
mm <mmm@zzz.com>
2019-12-13 23:09:49 +0900

    温度チェックを追加

--- organizer_svc.py
+++ organizer_svc.py
@@ -6,6 +6,40 @@ from flask import Flask
 app = Flask(__name__)
 
 
+@app.route('/measure_temperature', methods=['POST'])
+def post():
+
+    # sense temperature
+    response = requests.get(
+        'http://sensor:8080/'
+    )
+
+    # send temperature to m2x
+    response_post = requests.post(
+        'http://m2x:8080/m2x', None,
+        response.json()
+    )
+
+    return response_post.json()
+
+
+@app.route('/check_temperature', methods=['POST'])
+def post():
+
+    # check temperature
+    message = requests.post(
+        'http://tempcheck:8080/check_temp'
+    )
+
+    # send message thru line
+    response_post = requests.post(
+        'http://line:8080/send_msg/', None,
+        message.json()
+    )
+
+    return response_post.json()
+
+
 @app.route('/', methods=['POST'])
 def organize():
     # GETパラメータはparams引数に辞書で指定する
